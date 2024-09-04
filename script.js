0de016778f21acde392334fbd744eba0e7e9bc71
Iqra <iqraanzish48@gmail.com>
2024-09-04 20:08:44 +0500

    Add files via upload
    Currency_converter

---
+++ script.js
@@ -0,0 +1,20 @@
+const currency = {
+    USD:1,
+    EUR:0.91,
+    GBP: 0.76,
+    INR:74.57,
+    PKR: 280
+}
+document.getElementById('converter-form').addEventListener('submit',function(e) {
+    e.preventDefault();
+    let fromCurrency = document.getElementById('from').value
+    let ToCurrency = document.getElementById('To').value
+    let amount = document.getElementById('amount').value
+
+    let fromAmount = currency[fromCurrency]
+    let toAmount = currency[ToCurrency]
+    let baseAmount = amount / fromAmount
+    let convertedAmount = baseAmount * toAmount
+
+    document.getElementById('result').textContent = `Converted Amount:${Math.round(convertedAmount)}`
+})
